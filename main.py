a00fdae8fb0301e4927e822ce05b4fba6973955e
DartServant <sertern63@gmail.com>
2025-03-10 06:58:53 +0700

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os 
 import discord
 from discord.ext import commands
+from myserver import server_on  # เรียกฟังก์ชันที่รัน Flask server
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -11,6 +12,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f'✅ บอทออนไลน์แล้ว! : {bot.user}')
+    server_on()  # เรียกใช้งาน server ที่ทำงานใน background
 
 @bot.command()
 async def hello(ctx):
