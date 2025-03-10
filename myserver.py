6adb86877699217ea653f5570a1fbcd128e4f2e5
DartServant <sertern63@gmail.com>
2025-03-10 16:30:32 +0700

    myserver.py

--- myserver.py
+++ myserver.py
@@ -9,10 +9,9 @@ def home():
     return "Server is Running!"
 
 def run():
-    
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 10000))  # Render ใช้ PORT นี้เป็นค่าเริ่มต้น
     app.run(host='0.0.0.0', port=port)
 
 def server_on():
-    t = Thread(target=run)
+    t = Thread(target=run, daemon=True)  # ใช้ daemon เพื่อให้ปิดโปรแกรมได้ง่ายขึ้น
     t.start()
